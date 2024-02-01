93f4a2b15dbb89e2c0508e00f8c7dd111c6c58b9
Elman Seferli <itiselman@gmail.com>
2024-02-01 16:54:46 +0400

    first commit

--- industrialists/app/urls.py
+++ industrialists/app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('team/', AboutUs.as_view(), name='team'),
     path('companies/', AllCompaniesView.as_view(), name='all_companies'),
-    path('categories/', AllCategoriesView.as_view(), name='all_categories'),  # Moved this line up
+    path('categories/', AllCategoriesView.as_view(), name='all_categories'),
     path('<str:company_name>/', CompanyDetailView.as_view(), name='company_detail'),
     path('category/<str:category_name>/', CategoryCompaniesView.as_view(), name='category_companies'),
 ]
